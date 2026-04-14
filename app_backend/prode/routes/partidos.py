d073b6d99958ae844001fc2d18bc364d28bf39dd
ezequiel <ezeblade@gmail.com>
2026-04-14 14:54:10 -0300

    feature: Creacion del get y post usuarios

--- app_backend/prode/routes/partidos.py
+++ app_backend/prode/routes/partidos.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app_backend.prode.db import get_connection
-
+from app_backend.prode.services import partidos as partidos_service
 # EJEMPLO cuando usen validar o servicios 
 #from app_backend.prode.validators.partidos import validar_listado_partidos
 #from app_backend.prode.services.partidos import listar_partidos
@@ -10,20 +10,9 @@ partidos_bp = Blueprint("partidos", __name__)
 # Endpoints Partidos
 
 @partidos_bp.route("/")
-def get_partidos():
-    conn = get_connection()
-    cursor = conn.cursor(dictionary=True)
-    cursor.execute("""
-        SELECT p.id,
-               el.nombre AS equipo_local,
-               ev.nombre AS equipo_visitante,
-               p.fecha_partido AS fecha,
-               p.fase_torneo AS fase
-        FROM partido p
-        JOIN equipo el ON p.id_equipo_local = el.id
-        JOIN equipo ev ON p.id_equipo_visitante = ev.id
-    """)
-    partidos = cursor.fetchall()
-    cursor.close()
-    conn.close()
+def listar_partidos():
+    partidos = partidos_service.listar_partidos()
+    if not partidos:
+        return "", 204
     return jsonify(partidos)
+
--- app_backend/prode/services/partidos.py
+++ app_backend/prode/services/partidos.py
@@ -1,1 +1,19 @@
 from app_backend.prode.db import get_connection
+
+def listar_partidos():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+        SELECT p.id,
+               el.nombre AS equipo_local,
+               ev.nombre AS equipo_visitante,
+               p.fecha_partido AS fecha,
+               p.fase_torneo AS fase
+        FROM partido p
+        JOIN equipo el ON p.id_equipo_local = el.id
+        JOIN equipo ev ON p.id_equipo_visitante = ev.id
+    """)
+    partidos = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return partidos
